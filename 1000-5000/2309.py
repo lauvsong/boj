d5af5afdd93ab93521492fbc433b9ca5ee4dc7ba
hyesong kim <wisepine@kakao.com>
2021-12-28 18:16:30 +0900

    Add 2309

---
+++ 1000-5000/2309.py
@@ -0,0 +1,20 @@
+import sys
+input = sys.stdin.readline
+
+def findSpy():
+    for x in range(9):
+        for y in range(x+1, 9):
+            result = heights[x] + heights[y]
+            if result == offset: return heights[x], heights[y]
+
+heights = [int(input().strip()) for _ in range(9)]
+offset = sum(heights) - 100
+
+spy1, spy2 = findSpy()
+heights.sort()
+
+cnt = 0
+for index, height in enumerate(heights):
+    if height == spy1 or height == spy2: continue
+    print(height)
+    cnt += height
